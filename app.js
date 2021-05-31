0d485535d0c7468dc08eaa3d23c491d5aa05dfea
Bisrat Kidane <bisrat.kidane@sozialinfo.ch>
2021-05-31 19:52:20 +0200

    moves graph insertion to helper from main

--- app.js
+++ app.js
@@ -20,46 +20,30 @@ lr.on('line', (line, lineCount) => {
         let cableInfo = line.split(',');
         let source = new Node(cableInfo[2], cableInfo[3]);
         let target = new Node(cableInfo[4], cableInfo[5]);
-        if(!nodes.some(el => el.id === source.id)) {
+        if (!nodes.some(el => el.id === source.id)) {
             nodes.push(source);
         }
-        if(!nodes.some(el => el.id === target.id)) {
+        if (!nodes.some(el => el.id === target.id)) {
             nodes.push(target);
         }
         let edge = new Edge(cableInfo[0], cableInfo[1], source.id, target.id);
         edges.push(edge);
         let cable = {
-            id : cableInfo[0], length : cableInfo[1], startNodeId : cableInfo[2],
-            startNodeType : cableInfo[3], endNodeId : cableInfo[4], endNodeType : cableInfo[5]}
+            id: cableInfo[0],
+            length: cableInfo[1],
+            startNodeId: cableInfo[2],
+            startNodeType: cableInfo[3],
+            endNodeId: cableInfo[4],
+            endNodeType: cableInfo[5]
+        }
         cables.push(cable);
     }
 })
 .on('close', () => {
-    let roots = helper.findRootsNodes(root, edges);
-    cables.forEach((elem) => {
-        let isRoot = false;
-        for (let i = 0; i < roots.length; i++) {
-            if (roots[i].source === elem.startNodeId) {
-                isRoot = true;
-            }
-        }
-        if (isRoot) {
-            let target = new Node(elem.endNodeId, elem.endNodeType);
-            if (!graph.nodes.has(target)) {
-                graph.addNode(target);
-            }
-            graph.addEdge(root, target, elem.length, elem.id);
-        } else {
-            let source = new Node(elem.startNodeId, elem.startNodeType);
-            let target = new Node(elem.endNodeId, elem.endNodeType);
-            graph.addNode(source);
-            graph.addNode(target);
-            graph.addEdge(source, target, elem.length, elem.id);
-        }
-    });
+    helper.findRootsNodes(root, edges, cables, graph);
     let topology = {
-        nodes : graph.getNodes(),
-        edges : graph.getEdges()
+        nodes: graph.getNodes(),
+        edges: graph.getEdges()
     };
     console.log(topology);
 })
--- helper.js
+++ helper.js
@@ -1,9 +1,11 @@
-function findRootsNodes(root, edges) {
+const Node = require('./node');
+
+function findRootsNodes(root, edges, cables, graph) {
     const roots = [];
     for (let i = 0; i < edges.length; i++) {
         let isEndNode = true;
         for (let j = 0; j < edges.length; j++) {
-            if(edges[i].source === edges[j].target) {
+            if (edges[i].source === edges[j].target) {
                 isEndNode = false;
             }
         }
@@ -11,7 +13,31 @@ function findRootsNodes(root, edges) {
             roots.push(edges[i])
         }
     }
-    return roots;
+    mergeRootNodes(root, roots, cables, graph);
+}
+
+function mergeRootNodes(root, roots, cables, graph) {
+    cables.forEach((elem) => {
+        let isRoot = false;
+        for (let i = 0; i < roots.length; i++) {
+            if (roots[i].source === elem.startNodeId) {
+                isRoot = true;
+            }
+        }
+        if (isRoot) {
+            let target = new Node(elem.endNodeId, elem.endNodeType);
+            if (!graph.nodes.has(target)) {
+                graph.addNode(target);
+            }
+            graph.addEdge(root, target, elem.length, elem.id);
+        } else {
+            let source = new Node(elem.startNodeId, elem.startNodeType);
+            let target = new Node(elem.endNodeId, elem.endNodeType);
+            graph.addNode(source);
+            graph.addNode(target);
+            graph.addEdge(source, target, elem.length, elem.id);
+        }
+    });
 }
 
 module.exports.findRootsNodes = findRootsNodes;
